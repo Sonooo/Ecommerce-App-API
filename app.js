4e76bfc94bd2330fc3ba74c0eb83d0c685c9c238
Saurabh Sanap <sanapsaurabh218@gmail.com>
2024-06-12 11:27:58 +0530

    new changes

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 // my routes
-
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
 const categoryRoutes = require("./routes/category");
 const productRoutes = require("./routes/product");
-
+const orderRoutes = require("./routes/order");
+const paymentBRoutes = require("./routes/paymentBRoutes");
 
 //DB connection
 mongoose
@@ -29,10 +31,12 @@ app.use(cookieParser());
 app.use(cors());
 
 //My Routes
-
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
 app.use("/api", categoryRoutes);
 app.use("/api", productRoutes);
-
+app.use("/api", orderRoutes);
+app.use("/api", paymentBRoutes);
 
 
 
